e2ee0b4955a1797dd3d3dbda12e25e5e595aa181
onesirian <ldlockhart@gmail.com>
2026-03-11 05:51:56 -0500

    feat: add skeleton spring boot project

---
+++ ldlockhart/src/main/java/com/gravitino/project/ldlockhart/LdlockhartApplication.java
@@ -0,0 +1,13 @@
+package com.gravitino.project.ldlockhart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdlockhartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdlockhartApplication.class, args);
+	}
+
+}
---
+++ ldlockhart/src/test/java/com/gravitino/project/ldlockhart/LdlockhartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gravitino.project.ldlockhart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdlockhartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
